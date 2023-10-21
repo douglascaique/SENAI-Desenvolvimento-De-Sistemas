7239f1ae60d866ee01b1901bfc2682a996a105b3
Douglas Caíque <60799530+douglascaique@users.noreply.github.com>
2023-10-21 12:25:42 -0300

    Add files via upload

---
+++ Aula21.10Douglas/python/hello.py
@@ -0,0 +1,15 @@
+ola_Mundo = True
+if ola_Mundo:
+  print("hello world!")
+
+
+
+#comentário
+  spam = 1
+  text = "Olá caralho, sou uma string" 
+
+
+  # #calc
+
+  
+  
---
+++ Aula21.10Douglas/python/numeros.py
@@ -0,0 +1,21 @@
+#operadores
+# + - * /
+
+#soma
+print("Soma")
+print(20+4)
+soma = 75 + 69
+print(soma)
+
+
+#subtração
+print("Subtração")
+print(100 - 70)
+
+#multiplicação
+print("Multiplicação")
+print(10*50)
+
+#divisao
+print("Divisão")
+print(200/4)
