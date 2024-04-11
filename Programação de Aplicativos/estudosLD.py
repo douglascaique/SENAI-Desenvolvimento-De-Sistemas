9319dfddf66cf0d66005478b82b6c8f0067b0f49
Douglas Caíque <douglascaique@hotmail.com>
2024-04-11 00:02:45 -0300

    att senai

---
+++ Programação de Aplicativos/estudosLD.py
@@ -0,0 +1,22 @@
+
+# print('A','vida', 'é', 'bela', sep="-")
+
+
+# print('Programando','em',sep='...')
+# print('***','Python', '***')
+
+# print('Programando...em***Python***')
+
+d = 5.385
+c = '$'
+lista = [5,6,7,8,9]
+
+
+# for p in lista:
+#     print(p*d)
+
+# for p in range(4,16):print(p,p*d)
+
+# marcador de posição %f
+msg = 'Um dólar vale %f reais'
+print(msg %d)
