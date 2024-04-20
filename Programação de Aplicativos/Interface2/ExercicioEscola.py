77aabf6659dbc40d146cca69534efea5e0cb0011
douglascaique <douglascaique@hotmail.com>
2024-04-20 11:19:12 -0300

    Limpeza e Resultado do app em um entry vazio

---
+++ Programação de Aplicativos/Interface2/ExercicioEscola.py
@@ -0,0 +1,96 @@
+import customtkinter
+from tkinter import *
+
+
+
+
+app = customtkinter.CTk('#015F03') #cria e da cor para janela
+app.geometry("1000x800")
+app.title('Sistema Escolar V 1.0')
+
+
+
+# FunçãoResultado
+
+def calculaMedia():
+    media = (float(input1Unidade.get()) + float(input2Unidade.get()) + float(input3Unidade.get()))/3
+    return media
+
+
+def resultadoMedia():
+    media = calculaMedia()
+    if (media >= 7):
+        saidaResult.insert(0, f'A média do aluno {inputNome.get()} é de {media:.2}. Você foi APROVADO')
+    else:
+        saidaResult.insert("Recuperação", f'A média do aluno {inputNome.get()} é de {media:.2}')
+    
+        
+
+
+
+
+
+#labels/input
+customtkinter.CTkLabel(app,
+                       text="Sistema Escolar V 1.0",
+                       font=('Istok Web', 58, "bold"),
+                       text_color='#FFFFFF').pack()
+
+
+#InputName
+
+
+inputNome = customtkinter.CTkEntry(app,
+                                   placeholder_text='Digite o seu nome',
+                                   width=400)
+inputNome.pack(pady=10)
+
+
+#Nota1Unidade
+
+input1Unidade = customtkinter.CTkEntry(app,
+                                       placeholder_text='Digite sua nota da 1ª Unidade',
+                                       width=400)
+input1Unidade.pack(pady=10)
+
+#Nota2Unidade
+
+
+input2Unidade = customtkinter.CTkEntry(app,
+                                       placeholder_text='Digite sua nota da 2ª Unidade',
+                                       width=400)
+input2Unidade.pack(pady=10)
+
+
+#Nota3Unidade
+
+input3Unidade = customtkinter.CTkEntry(app, placeholder_text='Digite sua nota da 3ª Unidade', width=400)
+input3Unidade.pack(pady=10)
+
+
+
+# ButtonResult
+buttonResult = customtkinter.CTkButton(app, text="Resultado",
+                                       font=('Istok Web', 20, "bold"),
+                                       width=200, height=30,
+                                       text_color='White',
+                                       bg_color='#2FB201',
+                                       command=resultadoMedia)
+buttonResult.pack(pady=10)
+
+# ButtonLimpar
+buttonLimpar = customtkinter.CTkButton(app, text="Limpar",
+                                       font=('Istok Web', 20, "bold"),
+                                       width=200, height=30,
+                                       text_color='White',
+                                       bg_color='red',
+                                       command=resultadoMedia)
+buttonLimpar.pack(pady=10)
+
+
+saidaResult = customtkinter.CTkEntry(app, width=400, height=100)
+saidaResult.pack(pady=30)
+
+
+# Roda app
+app.mainloop()
--- Programação de Aplicativos/Interface2/ExercicioEscola_Wplace.py
+++ Programação de Aplicativos/Interface2/ExercicioEscola_Wplace.py
@@ -32,44 +32,44 @@ def resultadoMedia():
 
 #labels/input
 customtkinter.CTkLabel(app, text="Sistema Escolar V 1.0", font=('Istok Web', 58, "bold"),
-text_color='#000000').pack(pady=10)
+text_color='#000000').place(x=100,y=1)
 
 
 #InputName
 customtkinter.CTkLabel(app, text="Nome:", font=('Istok Web', 25, "bold"),
-text_color='#000000').pack(pady=10)
+text_color='#000000').place(x=200,y=100)
 
 inputNome = customtkinter.CTkEntry(app, placeholder_text='Digite o seu nome', width=400)
-inputNome.pack()
+inputNome.place(x=200, y=130)
 
 
 #Nota1Unidade
 customtkinter.CTkLabel(app, text="1ª Unidade", font=('Istok Web', 25, "bold"),
-text_color='#000000').pack(pady=10)
+text_color='#000000').place(x=200, y=170)
 
 input1Unidade = customtkinter.CTkEntry(app, placeholder_text='Digite sua nota da 1ª Unidade', width=400)
-input1Unidade.pack()
+input1Unidade.place(x=200, y=200)
 
 #Nota2Unidade
 customtkinter.CTkLabel(app, text="2ª Unidade", font=('Istok Web', 25, "bold"),
-text_color='#000000').pack(pady=10)
+text_color='#000000').place(x=200, y=240)
 
 input2Unidade = customtkinter.CTkEntry(app, placeholder_text='Digite sua nota da 2ª Unidade', width=400)
-input2Unidade.pack()
+input2Unidade.place(x=200, y=270)
 
 
 #Nota3Unidade
 customtkinter.CTkLabel(app, text="3ª Unidade", font=('Istok Web', 25, "bold"),
-text_color='#000000').pack(pady=10)
+text_color='#000000').place(x=200, y=310)
 
 input3Unidade = customtkinter.CTkEntry(app, placeholder_text='Digite sua nota da 3ª Unidade', width=400)
-input3Unidade.pack()
+input3Unidade.place(x=200, y=340)
 
 
 
 # ButtonResult
 buttonResult = customtkinter.CTkButton(app, text="Resultado", font=('Istok Web', 20, "bold"), width=250, height=50, text_color='white', border_color='black', border_width=3, command=resultadoMedia)
-buttonResult.pack(pady=15)
+buttonResult.place(x=300, y=400)
 
 
 app.mainloop()
