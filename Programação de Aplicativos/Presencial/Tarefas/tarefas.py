7bfa02253afb7de4c9082b52345caf1bd33b15cb
douglas <douglascaique@hotmail.com>
2024-04-27 10:02:39 -0300

    Att senai

---
+++ Programação de Aplicativos/Presencial/Tarefas/tarefas.py
@@ -0,0 +1,26 @@
+import customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+
+ctk.set_appearance_mode('dark')
+app = ctk.CTk()
+app.geometry('350x450')
+app.title('Lista de Tarefas - By Douglas')
+
+#----root style
+fontH1 = ('Arial', 30, 'bold')
+fontH3 = ('Arial', 18, 'bold')
+fontP = ('Arial', 12, 'bold')
+
+
+
+
+
+
+
+
+
+
+
+
+app.mainloop()
